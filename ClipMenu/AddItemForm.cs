53e5dcd6f0184021576ae12b52186fc14c39b5e7
flarn2006 <flarn2006@gmail.com>
2014-05-29 18:19:55 -0400

    REAL initial commit :-)

---
+++ ClipMenu/AddItemForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipMenu
+{
+    public partial class AddItemForm : Form
+    {
+        public AddItemForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddItemForm_Load(object sender, EventArgs e)
+        {
+            string clipboardText = Clipboard.GetText();
+            if (!clipboardText.Contains('\n')) {
+                itemText.Text = clipboardText;
+                itemText.SelectAll();
+            }
+        }
+
+        /// <summary>
+        /// Displays a new AddItemForm to the user as a modal dialog box prompting for a string.
+        /// </summary>
+        /// <returns>The string the user entered, or null if the user pressed Cancel.</returns>
+        public static string AskForText()
+        {
+            AddItemForm form = new AddItemForm();
+            if (form.ShowDialog() == DialogResult.OK) {
+                return form.itemText.Text;
+            } else {
+                return null;
+            }
+        }
+
+        private void itemText_TextChanged(object sender, EventArgs e)
+        {
+            ok.Enabled = !itemText.Text.Equals("");
+        }
+    }
+}
---
+++ ClipMenu/RemoveItemsForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ClipMenu
+{
+    partial class RemoveItemsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkList = new System.Windows.Forms.CheckedListBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.selectAll = new System.Windows.Forms.Button();
+            this.selectNone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select the item or items to remove.";
+            // 
+            // checkList
+            // 
+            this.checkList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkList.CheckOnClick = true;
+            this.checkList.FormattingEnabled = true;
+            this.checkList.IntegralHeight = false;
+            this.checkList.Location = new System.Drawing.Point(12, 32);
+            this.checkList.Name = "checkList";
+            this.checkList.Size = new System.Drawing.Size(261, 220);
+            this.checkList.TabIndex = 1;
+            // 
+            // ok
+            // 
+            this.ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ok.Location = new System.Drawing.Point(279, 32);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(88, 23);
+            this.ok.TabIndex = 2;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(279, 61);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(88, 23);
+            this.cancel.TabIndex = 3;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // selectAll
+            // 
+            this.selectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectAll.Location = new System.Drawing.Point(279, 101);
+            this.selectAll.Name = "selectAll";
+            this.selectAll.Size = new System.Drawing.Size(88, 23);
+            this.selectAll.TabIndex = 4;
+            this.selectAll.Text = "Select &All";
+            this.selectAll.UseVisualStyleBackColor = true;
+            this.selectAll.Click += new System.EventHandler(this.selectAll_Click);
+            // 
+            // selectNone
+            // 
+            this.selectNone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectNone.Location = new System.Drawing.Point(279, 130);
+            this.selectNone.Name = "selectNone";
+            this.selectNone.Size = new System.Drawing.Size(88, 23);
+            this.selectNone.TabIndex = 4;
+            this.selectNone.Text = "Select &None";
+            this.selectNone.UseVisualStyleBackColor = true;
+            this.selectNone.Click += new System.EventHandler(this.selectNone_Click);
+            // 
+            // RemoveItemsForm
+            // 
+            this.AcceptButton = this.ok;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel;
+            this.ClientSize = new System.Drawing.Size(379, 264);
+            this.Controls.Add(this.selectNone);
+            this.Controls.Add(this.selectAll);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.checkList);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(205, 203);
+            this.Name = "RemoveItemsForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Remove Items";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkList;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button selectAll;
+        private System.Windows.Forms.Button selectNone;
+    }
+}
